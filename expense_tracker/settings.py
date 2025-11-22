d7204dfcf890213ec16f35e1135bbe82088394a8
Saxam Rawat <saxamrawat@gmail.com>
2025-11-22 19:35:05 +0530

    Added Apps and Base Template Structure

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "accounts",
+    "categories",
+    "transactions",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -54,7 +57,7 @@ ROOT_URLCONF = "expense_tracker.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / 'templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
